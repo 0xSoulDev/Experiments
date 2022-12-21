124b88a1e0aa61bd84e215b232c8956a40d023c2
BlockCipher <104627284+0xSoulDev@users.noreply.github.com>
2022-12-22 00:46:39 +0530

    Update BlockHiveFractionRentRegistry.sol

--- capstone/contracts/BlockHiveFractionRentRegistry.sol
+++ capstone/contracts/BlockHiveFractionRentRegistry.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import './FractionToken.sol';
 
-contract FractionRentRegistry is Ownable {
+contract BlockHiveFractionRentRegistry is Ownable {
 
     using SafeERC20 for IERC20;
 

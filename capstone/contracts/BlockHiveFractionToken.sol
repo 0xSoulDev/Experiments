f94450cf6d7b7ab820dedb1d89b3facbf686edd9
BlockCipher <104627284+0xSoulDev@users.noreply.github.com>
2022-12-22 00:46:21 +0530

    Update BlockHiveFractionToken.sol

--- capstone/contracts/BlockHiveFractionToken.sol
+++ capstone/contracts/BlockHiveFractionToken.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract FractionToken is ERC20, ERC20Burnable {
+contract BlockHiveFractionToken is ERC20, ERC20Burnable {
     uint256 public NFTId;
     address public NFTOwner;
     address public NFTAddress;
